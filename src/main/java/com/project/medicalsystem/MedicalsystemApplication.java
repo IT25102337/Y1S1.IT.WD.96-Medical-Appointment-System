157e5e2f83afa055c1934dd135f30a34e3a602b8
Chanithu Dilnuka <it25102337@my.sliit.lk>
2026-05-15 15:16:21 +0530

    Update files

---
+++ src/main/java/com/project/medicalsystem/MedicalsystemApplication.java
@@ -0,0 +1,9 @@
+package com.project.medicalsystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MedicalsystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalsystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/medicalsystem/MedicalSystemUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medicalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSystemUpdateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
